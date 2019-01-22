e2fc62bd8eb48c18a88d94ac9a6d3a950edf3c92
briandsk <44901467+Briansz@users.noreply.github.com>
2019-01-22 18:59:53 -0300

    Add files via upload

---
+++ Tabuada 1.py
@@ -0,0 +1,19 @@
+print("")
+print("################################# \n             Tabuada \n#################################")
+print("")
+num = int(input("Digite um numero pra saber a tabuada: "))
+tab1 = num*1 
+tab2 = num*2
+tab3 = num*3
+tab4 = num*4
+tab5 = num*5
+tab6 = num*6
+tab7 = num*7
+tab8 = num*8
+tab9 = num*9
+tab10 = num*10
+print("")
+print("------------[*]")
+print("{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {} \n{} x {:2} = {}"
+	.format(num, 1, tab1, num, 2, tab2, num, 3, tab3, num, 4, tab4, num, 5, tab5, num, 6, tab6, num, 7, tab7, num, 8, tab8, num, 9, tab9, num, 10, tab10))
+print("------------[*]")
